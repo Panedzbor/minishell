778b7211bc641ab9f34c19599f7fbf5ab1d4b32a
plantsov <plantsov@student.42berlin.de>
2025-08-29 16:19:45 +0200

    FIX env and refactoring

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,39 +1,6 @@
 #include "../includes/minishell.h"
 
-t_tree_node *parser(char *input)
-{
-	t_token *tokens;
-	t_tree_node *tree;
-
-	tokens = lexer(input);
-	//test_print_tokens(tokens);
-	if (!analyze_parenthesis(tokens, 0))
-		printf("Error\n");
-	tree = fill_tree(tokens, NULL);
-	return (tree);
-}
-
-t_tree_node *fill_tree(t_token *start, t_token *end)
-{
-    t_tree_node *tree;
-    t_token *left_branch[2];
-    t_token *right_branch[2];
-    t_token *parent;
-
-    if (!start)
-        return (NULL);
-    ft_bzero(left_branch, 2 * sizeof(t_token *));
-    ft_bzero(right_branch, 2 * sizeof(t_token *));
-    if (!end)
-        end = find_list_end(start);
-    parent = divide_tokens(start, end, left_branch, right_branch);
-    tree = create_tree_node(parent);
-    tree->left = fill_tree(left_branch[0], left_branch[1]);
-    tree->right = fill_tree(right_branch[0], right_branch[1]);
-    return (tree);
-}
-
-t_token *find_list_end(t_token *start)
+static t_token *find_list_end(t_token *start)
 {
 	t_token *current;
 
@@ -78,4 +45,38 @@ t_token *find_lowest_priority(t_token *start, t_token *end)
 	if (current->priority <= lowest->priority)
 		lowest = current;
 	return (lowest);
+}
+
+static t_tree_node *fill_tree(t_token *start, t_token *end)
+{
+    t_tree_node *tree;
+    t_token *left_branch[2];
+    t_token *right_branch[2];
+    t_token *parent;
+
+    if (!start)
+        return (NULL);
+    ft_bzero(left_branch, 2 * sizeof(t_token *));
+    ft_bzero(right_branch, 2 * sizeof(t_token *));
+    if (!end)
+        end = find_list_end(start);
+    parent = divide_tokens(start, end, left_branch, right_branch);
+    tree = create_tree_node(parent);
+    tree->left = fill_tree(left_branch[0], left_branch[1]);
+    tree->right = fill_tree(right_branch[0], right_branch[1]);
+    return (tree);
+}
+
+t_tree_node *parser(char *input)
+{
+	t_token *tokens;
+	t_tree_node *tree;
+
+	tokens = lexer(input);
+	//test_print_tokens(tokens);
+	if (!analyze_parenthesis(tokens, 0))
+		printf("Error\n");
+	tree = fill_tree(tokens, NULL);
+	//free tokens
+	return (tree);
 }
--- srcs/test_functions.c
+++ srcs/test_functions.c
@@ -1,17 +1,5 @@
 #include "../includes/minishell.h"
 
-void test_list_data(t_node *test_node)
-{
-	int i;
-
-	i = 1;
-	while (test_node)
-	{
-		ft_printf("INFO:  Node #%d: %s\n", i, test_node->command[0]);
-		test_node = test_node->next;
-		i++;
-	}
-}
 void test_print_tokens(t_token *head)
 {
 	int i;
